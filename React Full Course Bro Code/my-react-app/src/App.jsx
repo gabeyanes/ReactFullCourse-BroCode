3726fb1a66724512de6c304b7ccc629490372c07
GabeYanes1217 <gabriel.yanes1217@outlook.com>
2024-12-03 20:00:21 -0300

    Commit 1

---
+++ React Full Course Bro Code/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./header"
+import Footer from "./Footer"
+import Food from "./Food"
+
+function App() {
+
+  return(
+    <>
+    <Header></Header>
+    <Food></Food>
+    <Footer></Footer>
+    </>
+    
+  );
+}
+
+export default App
